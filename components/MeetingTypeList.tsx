efbccd12b26683d8403264896f0805772dfcd75f
Kacper Lechicki <klechicki.wrk@gmail.com>
2025-08-23 17:25:45 +0200

    feat: instant meeting modal

--- components/MeetingTypeList.tsx
+++ components/MeetingTypeList.tsx
@@ -3,6 +3,7 @@
 import React, { useState } from 'react';
 import HomeCard from './HomeCard';
 import { useRouter } from 'next/navigation';
+import MeetingModal from './MeetingModal';
 
 const MeetingTypeList = () => {
   const router = useRouter();
@@ -11,13 +12,15 @@ const MeetingTypeList = () => {
     'isScheduleMeeting' | 'isJoiningMeeting' | 'isInstantMeeting' | undefined
   >();
 
+  const createMeeting = () => {};
+
   return (
     <section className="grid grid-cols-1 gap-5 lg:grid-cols-2 2xl:grid-cols-4">
       <HomeCard
         img="/icons/add-meeting.svg"
         title="New Meeting"
         description="Start an instant meeting"
-        handleClick={() => setMeetingState('isJoiningMeeting')}
+        handleClick={() => setMeetingState('isInstantMeeting')}
         className="bg-orange-1"
       />
 
@@ -43,6 +46,14 @@ const MeetingTypeList = () => {
         description="via invitation link"
         handleClick={() => setMeetingState('isJoiningMeeting')}
         className="bg-yellow-1"
+      />
+
+      <MeetingModal
+        isOpen={meetingState === 'isInstantMeeting'}
+        onClose={() => setMeetingState(undefined)}
+        title="Start an Instant Meeting"
+        buttonText="Start Meeting"
+        handleClick={createMeeting}
       />
     </section>
   );
